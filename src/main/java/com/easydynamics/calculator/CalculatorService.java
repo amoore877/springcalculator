32c2ff64b91aba1a1f92d79452082d0488a34351
amoore877 <amoore877@gmail.com>
2018-07-14 08:25:35 -0400

    Initialize

---
+++ src/main/java/com/easydynamics/calculator/CalculatorService.java
@@ -0,0 +1,24 @@
+package com.easydynamics.calculator;
+
+public class CalculatorService {
+    public static String add(String a, String b) {
+        // TODO: Finish method
+        throw new UnsupportedOperationException();
+    }
+
+    public static String subtract(String a, String b) {
+        // TODO: Finish method
+        throw new UnsupportedOperationException();
+    }
+
+    public static String multiply(String a, String b) {
+        // TODO: Finish method
+        throw new UnsupportedOperationException();
+    }
+
+    public static String divide(String a, String b) {
+        // TODO: Finish method
+        throw new UnsupportedOperationException();
+    }
+
+}
